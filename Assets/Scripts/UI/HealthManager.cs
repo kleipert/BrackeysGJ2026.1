12ed7d2853a491f5f2c02cb9d825e684047b50b8
Banbaro <leon_baur@hotmail.de>
2026-02-15 16:23:01 +0100

    [ADD] Player UI

---
+++ Assets/Scripts/UI/HealthManager.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private Image[] healthBar;
+
+    private int currentHealth;
+    private int maxHealth;
+
+    public static HealthManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        currentHealth = 3;
+        maxHealth = 3;
+        
+        for (int i = maxHealth; i < healthBar.Length; i++)
+        {
+            if (healthBar[i] != null)
+                healthBar[i].enabled = false;
+        }
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    public void TakeDamage(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - amount, 0, maxHealth);
+        
+        for (int i = 0; i < maxHealth; i++)
+        {
+            if (healthBar[i] == null) continue;
+
+            var anim = healthBar[i].GetComponent<Animator>();
+            if (anim != null)
+                anim.SetBool("Lost", i >= currentHealth);
+        }
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    public void Heal(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+
+        for (int i = 0; i < maxHealth; i++)
+        {
+            if (healthBar[i] == null) continue;
+
+            var anim = healthBar[i].GetComponent<Animator>();
+            if (anim != null)
+                anim.SetBool("Lost", i >= currentHealth);
+        }
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    public void IncreaseHealth()
+    {
+        if (maxHealth >= healthBar.Length) return;
+        
+        if (healthBar[maxHealth] != null)
+            healthBar[maxHealth].enabled = true;
+
+        maxHealth++;
+        currentHealth = maxHealth;
+        
+        for (int i = 0; i < maxHealth; i++)
+        {
+            if (healthBar[i] == null) continue;
+
+            var anim = healthBar[i].GetComponent<Animator>();
+            if (anim != null)
+                anim.SetBool("Lost", false);
+        }
+    }
+}
