8ccb1c5fb90057269aaeea1a056cbca50b9efe1e
Kevin <kleipert@web.de>
2026-02-18 19:09:06 +0100

    [FIX] brain boss fixes and overhaul

--- Assets/Scripts/Bosses/BrainBoss.cs
+++ Assets/Scripts/Bosses/BrainBoss.cs
@@ -1,20 +1,27 @@
-using GameManager;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Bosses
 {
     public class BrainBoss : MonoBehaviour
     {
-        [SerializeField] private int _attacksBeforeWeak = 3;
+        [SerializeField] private int _attacksBeforeWeakMax = 6;
         [SerializeField] private int _attacksLeft;
         [SerializeField] private int _healthTotal = 3;
         [SerializeField] private int _healthCurrent;
         [SerializeField] private float _movementSpeed = 5f;
+        [SerializeField] private float _movementSpeedMax = 5f;
         [SerializeField] private GameObject _lightningPrefab;
+        [SerializeField] private Transform[] _lightningPositionsGround;
+        [SerializeField] private Transform[] _lightningPositionsPlatforms;
+        private Transform[] _pickedPositionsGround;
+        private Transform[] _pickedPositionsPlatforms;
 
         
         private bool _isIdle = true;
-        [SerializeField] private float _idleTimeBetweenAttacks = 5f;
+        [SerializeField] private float _idleTimeBetweenAttacksMin = 5f;
+        [SerializeField] private float _idleTimeBetweenAttacks;
         [SerializeField] private float _idleTimeBetweenAttacksLeft;
         [SerializeField] private Transform _idlePositionLeft;
         [SerializeField] private Transform _idlePositionRight;
@@ -22,16 +29,17 @@ namespace Bosses
         [SerializeField] private bool _isMovingLeft = true;
         
         
-        private bool _isMovingToRecover = false;
-        
-        
-        private bool _isRecovering = false;
         [SerializeField] private float _recoveryTime = 5f;
         [SerializeField] private float _recoveryTimeLeft;
         [SerializeField] private Transform _recoveryPosition;
         
         
-        private bool _isMovingFromRecover = false;
+        [SerializeField] private bool _isMovingToRecover = false;
+        [SerializeField] private bool _isRecovering = false;
+        [SerializeField] private bool _isMovingFromRecover = false;
+        [SerializeField] private bool _isInIdleMiddle = true;
+        [SerializeField] private bool _isInRecoveryZone = false;
+        
 
 
         [SerializeField] private bool _playerFound = false;
@@ -45,9 +53,17 @@ namespace Bosses
         {
             _anim = GetComponent<Animator>();
             _healthCurrent = _healthTotal;
-            _attacksLeft = _attacksBeforeWeak;
+            
+            _attacksLeft = _attacksBeforeWeakMax - _healthCurrent;
+            _movementSpeed = _movementSpeedMax - _healthCurrent;
+            _idleTimeBetweenAttacks = _idleTimeBetweenAttacksMin + _healthCurrent;
+            
+            
             _idleTimeBetweenAttacksLeft = _idleTimeBetweenAttacks;
+
             _recoveryTimeLeft = _recoveryTime;
+            _pickedPositionsGround = new Transform[8];
+            _pickedPositionsPlatforms = new Transform[3];
             
             StartIdleAnimation();
         }
@@ -66,26 +82,49 @@ namespace Bosses
                 MoveFromRecover();
         }
 
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            switch (other.gameObject.name)
+            {
+                case "BossIdleLeft":
+                    _isMovingLeft = false;
+                    transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
+                    break;
+                case "BossIdleMiddle":
+                    _isInIdleMiddle = true;
+                    break;
+                case "BossIdleRight":
+                    _isMovingLeft = true;
+                    transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
+                    break;
+                case "BossRecovery":
+                    _isInRecoveryZone = true;
+                    break;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            switch (other.gameObject.name)
+            {
+                case "BossIdleMiddle":
+                    _isInIdleMiddle = false;
+                    break;
+                case "BossRecovery":
+                    _isInRecoveryZone = false;
+                    break;
+            }
+        }
+
         private void Idle()
         {
             if (_isMovingLeft)
             {
                 transform.Translate(Vector3.left * (_movementSpeed * Time.deltaTime));
-                if (Vector3.Distance(_idlePositionLeft.position, transform.position) <= 0.1f)
-                {
-                    _isMovingLeft = false;
-                    transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
-                }
-                    
             }
             else
             {
                 transform.Translate(Vector3.right * (_movementSpeed * Time.deltaTime));
-                if (Vector3.Distance(_idlePositionRight.position, transform.position) <= 0.1f)
-                {
-                    _isMovingLeft = true;
-                    transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
-                }
             }
             
             _idleTimeBetweenAttacksLeft -= Time.deltaTime;
@@ -93,9 +132,64 @@ namespace Bosses
             {
                 if (_attacksLeft >= 1)
                 {
+                    PickLightningPositions();
                     Attack();
                     _idleTimeBetweenAttacksLeft = _idleTimeBetweenAttacks;
                 }
+            }
+        }
+
+        private void PickLightningPositions()
+        {
+            List<int> indices = new List<int>();
+            int index;
+            var itemsFromLeft = 3;
+            var itemsFromMiddle = 2;
+            var itemsFromRight = 3;
+            var itemsFromPlatform = 3;
+            var random = new System.Random();
+            
+            for (int i = 0; i < itemsFromLeft; i++)
+            {
+                do 
+                    index = random.Next(0, 3);
+                while(indices.Contains(index));
+                    indices.Add(index);
+            }
+            
+            for (int i = 0; i < itemsFromMiddle; i++)
+            {
+                do 
+                    index = random.Next(4, 6);
+                while(indices.Contains(index));
+                indices.Add(index);
+            }
+            
+            for (int i = 0; i < itemsFromRight; i++)
+            {
+                do 
+                    index = random.Next(7, 11);
+                while(indices.Contains(index));
+                    indices.Add(index);
+            }
+
+            for (int i = 0; i < _pickedPositionsGround.Length; i++)
+            {
+                _pickedPositionsGround[i] = _lightningPositionsGround[indices[i]];
+            }
+            indices.Clear();
+            
+            for (int i = 0; i < itemsFromPlatform; i++)
+            {
+                do 
+                    index = random.Next(0, 3);
+                while(indices.Contains(index));
+                    indices.Add(index);
+            }
+
+            for (int i = 0; i < _pickedPositionsPlatforms.Length; i++)
+            {
+                _pickedPositionsPlatforms[i] = _lightningPositionsPlatforms[indices[i]];
             }
         }
 
@@ -104,16 +198,21 @@ namespace Bosses
             _isIdle = false;
             StartAttackAnimation();
             _attacksLeft--;
-            Debug.Log($"Performed Attack! Attacks left: {_attacksLeft}");
         }
 
         private void AttackDone()
         {
-
-            var targetPos = GameManagerSingleton.Instance.GetPlayerGroundCheckTransform().position;
-            targetPos.y -= 0.75f; 
-            var lightning = Instantiate(_lightningPrefab, targetPos, Quaternion.identity);
-            lightning.GetComponent<LightningImpact>().SetLightningMode(_healthCurrent);
+            foreach (var trans in _pickedPositionsGround)
+            {
+                var lightning = Instantiate(_lightningPrefab, trans.position, Quaternion.identity);
+                lightning.GetComponent<LightningImpact>().SetLightningMode(_healthCurrent);
+            }
+            
+            foreach (var trans in _pickedPositionsPlatforms)
+            {
+                var lightning = Instantiate(_lightningPrefab, trans.position, Quaternion.identity);
+                lightning.GetComponent<LightningImpact>().SetLightningMode(_healthCurrent);
+            }
             _isIdle = true;
             StartIdleAnimation();
             if (_attacksLeft == 0)
@@ -123,22 +222,27 @@ namespace Bosses
             }
         }
 
+        private bool recoveryXReached = false;
+        private bool recoveryYReached = false;
+        
+
         private void MoveToRecover()
         {
-            var correctPosX = Mathf.Abs(_recoveryPosition.position.x - transform.position.x) <= 0.1f;
-            var correctPosY = Mathf.Abs(_recoveryPosition.position.y - transform.position.y) <= 0.1f;
+            if(recoveryXReached == false)
+                recoveryXReached = _isInIdleMiddle;
             
-            // Move to correct X
-            if(!correctPosX && transform.position.x > _recoveryPosition.position.x)
+            if(recoveryYReached == false)
+                recoveryYReached = _isInRecoveryZone;
+            
+            if(!recoveryXReached && transform.position.x > _recoveryPosition.position.x)
                 transform.Translate(Vector3.left * (_movementSpeed * Time.deltaTime));
-            else if(!correctPosX && transform.position.x < _recoveryPosition.position.x)
+            else if(!recoveryXReached && transform.position.x < _recoveryPosition.position.x)
                 transform.Translate(Vector3.right * (_movementSpeed * Time.deltaTime));
             
-            // Move to correct Y
-            if(correctPosX && !correctPosY)
+            if(recoveryXReached && !recoveryYReached)
                 transform.Translate(Vector3.down * (_movementSpeed * Time.deltaTime));
 
-            if (correctPosX && correctPosY)
+            if (recoveryXReached && recoveryYReached)
             {
                 _isMovingToRecover = false;
                 _isRecovering = true;
@@ -154,14 +258,23 @@ namespace Bosses
                 _recoveryTimeLeft = _recoveryTime;
                 _isRecovering = false;
                 StartIdleAnimation();
-                _attacksLeft = _attacksBeforeWeak;
+                
+                _attacksLeft = _attacksBeforeWeakMax - _healthCurrent;
+                _movementSpeed = _movementSpeedMax - _healthCurrent;
+                _idleTimeBetweenAttacks = _idleTimeBetweenAttacksMin + _healthCurrent;
+                
+                _idleTimeBetweenAttacksLeft = _idleTimeBetweenAttacks;
+
+
+                recoveryXReached = false;
+                recoveryYReached = false;
                 _isMovingFromRecover = true;
             }
         }
 
         private void MoveFromRecover()
         {
-            var correctPosY = Mathf.Abs(_idlePositionMiddle.position.y - transform.position.y) <= 0.1f;
+            var correctPosY = _isInIdleMiddle;
             
             if(!correctPosY)
                 transform.Translate(Vector3.up * (_movementSpeed * Time.deltaTime));
