f19b30b416f30a25ee720db050ecf131f416eaf4
Banbaro <leon_baur@hotmail.de>
2026-02-18 16:42:36 +0100

    [ADD] Eye Background + Boss Sprities Heart and Eye

---
+++ Assets/Scripts/Player/PlayerSplit.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerSplit : MonoBehaviour
+{
+    [SerializeField] GameObject playerImage;
+    [SerializeField] float splitTimeScale = 0.25f;
+
+    private const float baseFixedDeltaTime = 0.02f;
+    private bool _isActivated;
+    private bool _corountineStarted;
+    private GameObject _clone;
+
+    private void Update()
+    {
+        if (_isActivated && !_corountineStarted) StartCoroutine(CountdownEvent());
+    }
+
+    public void OnSplit()
+    {
+        if (_isActivated) return;
+
+        _isActivated = true;
+        _clone = Instantiate(playerImage, transform.position, Quaternion.identity);
+
+        Time.timeScale = splitTimeScale;
+        Time.fixedDeltaTime = baseFixedDeltaTime * Time.timeScale;
+    }
+
+    IEnumerator CountdownEvent()
+    {
+        _corountineStarted = true;
+        yield return new WaitForSeconds(1);
+        
+        if (_clone) Destroy(_clone);
+        
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = baseFixedDeltaTime;
+        _isActivated = false;
+        _corountineStarted = false;
+    }
+}
